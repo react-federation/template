2a0817732a1c81b3fc4462c32043916e036bc7e8
Leonel Vieyra <me@leonelv.com>
2021-10-27 20:14:50 -0300

    test(ErrorFallback): add missing await

--- components/error-fallback/index.spec.tsx
+++ components/error-fallback/index.spec.tsx
@@ -2,11 +2,11 @@ import setup from 'utils/test/setup'
 import ErrorFallback from '.'
 
 describe('ErrorFallback', () => {
-  it('should match snapshot', () => {
+  it('should match snapshot', async () => {
     const resetErrorBoundary = jest.fn()
     const { findByText } = setup(ErrorFallback, { error: new Error('test error'), resetErrorBoundary })
 
-    const element = findByText("test error")
+    const element = await findByText("test error")
 
     expect(element).toBeInTheDocument()
   })
